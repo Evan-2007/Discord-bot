ef54d070555510aa92fa2505817228c0cb975740
Evan-2007 <65788884+Evan-2007@users.noreply.github.com>
2022-11-07 21:00:04 -0800

    created slash command and ping

---
+++ src/bot.js
@@ -0,0 +1,52 @@
+const { REST } = require('@discordjs/rest');
+require("dotenv").config();
+const { Client, GatewayIntentBits, Routes } = require("discord.js");
+
+const TOKEN = process.env.TOKEN
+const CLIENT_ID = process.env.CLIENT_ID
+const GUILD_ID = process.env.GUILD_ID
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+})
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+client.login(TOKEN);
+
+client.on('ready', ()=> {
+    console.log('the bot is ready')
+})
+
+client.on('messageCreate', message =>{
+    if (message.content === 'ping') {
+        message.reply('pong')
+    }
+})
+
+async function main() {
+    const commands = [
+        {
+            name: "test",
+            description: "test command"
+        },
+    ];
+    try{
+        console.log('Started refreshing application (/) commands.');
+        Routes.applicationGuildCommands
+        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { 
+            body: commands
+        });
+        client.login(TOKEN);
+    } catch (error){
+        console.log(error)
+    }
+}
+
+main();
+
+
